183c98c2bfc9b2f0688a88110700e94364ea71fa
sabrinaswatee <sabrina.swatee@gmail.com>
2017-06-19 10:09:21 +1000

    initial commit of treasure finder

--- treasure_hunter/treasure_hunter.js
+++ treasure_hunter/treasure_hunter.js
@@ -13,35 +13,19 @@ var map1 = [
 ["A","A","A","A","A"],
 ["A","A","A","A","A"]
 ];
-
 // Sample output:
 treasureFinder(map1) // should return [2, 2];
 You can assume that the size of the treasure map will be the same.
 
 Some additional maps:
-var map2 = [
-["A","A","A","A","A"],
-["A","A","A","A","A"],
-["A","A","A","A","A"],
-["A","A","A","A","A"],
-["A","X","A","A","A"]
-];
+var map2 = [                  var map3 = [                  var map4 = [
+["A","A","A","A","A"],        ["A","A","A","A","A"],        ["A","A","A","A","A"],
+["A","A","A","A","A"],        ["A","A","V","A","A"],        ["A","A","V","A","A"],
+["A","A","A","A","A"],        ["A","A","A","V","A"],        ["A","A","A","V","A"],
+["A","A","A","A","A"],        ["v","v","V","v","X"],        ["v","v","V","v","A"],
+["A","X","A","A","A"]         ["A","V","A","A","A"]         ["X","V","A","A","A"]
+];                            ];                            ];
 
-var map3 = [
-["A","A","A","A","A"],
-["A","A","V","A","A"],
-["A","A","A","V","A"],
-["v","v","V","v","X"],
-["A","V","A","A","A"]
-];
-
-var map4 = [
-["A","A","A","A","A"],
-["A","A","V","A","A"],
-["A","A","A","V","A"],
-["v","v","V","v","A"],
-["X","V","A","A","A"]
-];
 */
 
 var treasureFinder = function (bigArray) {
@@ -83,11 +67,9 @@ var holyGrailMap = [
 ];
 
 //sample outputs:
-
-treasureFinder(holyGrailMap, "G") // should return [4, 2];
+streasureFinder(holyGrailMap, "G") // should return [4, 2];
 treasureFinder(holyGrailMap, "H") // should return [5, 2];
 treasureFinder(holyGrailMap, "H") // should return [5, 2];
-
 treasureFinder(holyGrailMap, "V") // should return 'that doesnt belong in a museum!';
 */
 
