dd948dd7645f84d59e7f79980eb1a4f18a007b7d
sabrinaswatee <sabrina.swatee@gmail.com>
2017-06-28 13:42:28 +1000

    dice ruby

--- dice/dice.rb
+++ dice/dice.rb
@@ -36,16 +36,39 @@ require 'pry'
 class Dice
 
   @@rolls = []
+  @@dice_face = [
+    ["|   |", "| * |", "|   |"],
+    ["|   |", "|* *|", "|   |"],
+    ["|   |", "|***|", "|   |"],
+    ["|* *|", "|   |", "|* *|"],
+    ["|* *|", "| * |", "|* *|"],
+    ["|* *|", "|* *|", "|* *|"],
+    ["|* *|", "|***|", "|* *|"],
+    ["|***|", "|* *|", "|***|"],
+    ["|***|", "|***|", "|***|"],
+  ]
+
+  def self.print_dice (count)
+    counter = 0
+    puts "_____"
+    3.times do
+      puts @@dice_face[count - 1][counter]
+      counter += 1
+    end
+    puts "_____"
+  end
 
   def self.roll(num = 1)
     @@rolls = []
     random_array = []
     if num == 1
-      puts Random.rand(1..6)
-
+      random_num = Random.rand(1..6)
+      puts random_num
+      print_dice(random_num)
     else
       num.times do
-        random_array.push Random.rand(1..6)
+        randon_num = Random.rand(1..6)
+        random_array.push random_num
       end
       @@rolls.push random_array
       puts "#{random_array} \n"
