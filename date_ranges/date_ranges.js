a78e133f923494204e0191b8216581a3fe1d014b
sabrinaswatee <sabrina.swatee@gmail.com>
2017-08-09 15:07:27 +1000

    update date range

--- date_ranges/date_ranges.js
+++ date_ranges/date_ranges.js
@@ -18,10 +18,24 @@ makeFriendlyDates(["2016-07-01", "2018-07-04"]) should return ["July 1st, 2016",
 var dateArray1 = ["2016-07-01", "2016-07-04"];
 var dateArray2 = ["2016-07-01", "2018-07-04"];
 var dateArray3 = ["2017-07-01", "2017-07-04"];
-
-var daysArray = ['st', 'nd', 'rd', 'th'];
 var monthsArray = ["January", "February", "March", "April", "May", "June", "July",
                    "August", "September", "October", "November", "December"];
+
+
+function getDay (dateArray) {
+  if (dateArray[0][8] === '0' && dateArray[1][8] === '0') {
+   return [dateArray[0].slice(9, 10), dateArray[1].slice(9, 10)];
+  }
+  else if (dateArray[0][8] === '0' && dateArray[1][8] !== '0') {
+   return [dateArray[0].slice(9, 10), dateArray[1].slice(8, 10)];
+  }
+  else if (dateArray[0][8] !== '0' && dateArray[1][8] === '0') {
+   return [dateArray[0].slice(8, 10), dateArray[1].slice(9, 10)];
+  }
+  else {
+   return [dateArray[0].slice(8, 10), dateArray[1].slice(8, 10)];
+  }
+};
 
 function getOrdinal (day) {
   if (["1", "21", "31"].includes(day)) {
@@ -38,32 +52,24 @@ function getOrdinal (day) {
   }
 }
 
-function printDate (days, months, years, difference) {
-  var startOrdinal = getOrdinal(days[0]);
-  var endOrdinal = getOrdinal(days[1]);
-
+function printDate (days, months, years, difference, startOrdinal, endOrdinal) {
   if (difference < 365) {
     if (years[0] === years[1] && Number(years[0]) === new Date().getFullYear()) {
-      console.log ([monthsArray[months[0] - 1] + ' ' + days[0] + startOrdinal, days[1] + endOrdinal]);
+      return ([monthsArray[months[0] - 1] + ' ' + days[0] + startOrdinal, days[1] + endOrdinal]);
     }
     else {
-      console.log ([monthsArray[months[0] - 1] + ' ' + days[0] + startOrdinal + ', ' + years[0], days[1] + endOrdinal]);
+      return ([monthsArray[months[0] - 1] + ' ' + days[0] + startOrdinal + ', ' + years[0], days[1] + endOrdinal]);
     }
   }
   else {
     var startDate = monthsArray[months[0] - 1] + ' ' + days[0] + startOrdinal + ', ' + years[0];
     var endDate = monthsArray[months[1] - 1] + ' ' + days[1] + endOrdinal + ', ' + years[1];
-    console.log ([startDate, endDate]);
+    return ([startDate, endDate]);
   }
 };
 
 function makeFriendlyDates (dateArray) {
-  if (dateArray[0][8] === '0') {
-    var days = [dateArray[0].slice(9, 10), dateArray[1].slice(9, 10)];
-  }
-  else {
-    var days = [dateArray[0].slice(8, 10), dateArray[1].slice(8, 10)];
-  }
+  var days = getDay(dateArray);
   var months = [dateArray[0].slice(5, 7), dateArray[1].slice(5, 7)];
   var years = [dateArray[0].slice(0, 4), dateArray[1].slice(0, 4)];
 
@@ -72,7 +78,11 @@ function makeFriendlyDates (dateArray) {
   var endDate = new Date(years[1], months[1], days[1]);
   var difference = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / (oneDay)));
 
-  printDate(days, months, years, difference);
+  var startOrdinal = getOrdinal(days[0]);
+  var endOrdinal = getOrdinal(days[1]);
+  var output = printDate(days, months, years, difference, startOrdinal, endOrdinal);
+
+  console.log(output);
 }
 
 makeFriendlyDates(dateArray1);
